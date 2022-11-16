43681c604e9d457596c0d18489775715b16afd13
Aaron Key <aaronmikelkey@gmail.com>
2022-11-16 16:20:51 -0500

    Add example image and change links for use with flyio

--- index.js
+++ index.js
@@ -2,67 +2,69 @@ require('dotenv').config()
 
 const createError = require('http-errors')
 const path = require('path')
-const express = require('express');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const passport = require('passport');
+const express = require('express')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const passport = require('passport')
 const logger = require('morgan')
 
 // routes
 const indexRouter = require('./routes/index')
-const authRouter = require('./routes/passport');
+const authRouter = require('./routes/passport')
 
-const app = express();
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-const SESSION_SECRET = process.env.SESSION_SECRET;
+const SESSION_SECRET = process.env.SESSION_SECRET
 
 // Initialize Express and middlewares
 app.use(logger('dev'))
 app.use(express.json())
-app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }));
-app.use('/css', express.static(path.join(__dirname, '/css')));
+app.use(
+	session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false })
+)
+app.use('/css', express.static(path.join(__dirname, '/css')))
 app.use(cookieParser(SESSION_SECRET))
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Routes ect
 app.use('/', indexRouter)
 
 passport.serializeUser(function (user, done) {
-	done(null, user);
-});
+	done(null, user)
+})
 
 passport.deserializeUser(function (user, done) {
-	done(null, user);
-});
+	done(null, user)
+})
 
 app.use('/auth', authRouter)
 
-app.get('/logout', function(req, res){
-  req.logout();
-	res.clearCookie('userData').redirect('/');
-});
+app.get('/logout', function (req, res) {
+	req.logout()
+	res.clearCookie('userData').redirect('/')
+})
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+	next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message
+	res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+	// render the error page
+	res.status(err.status || 500)
+	res.render('error')
+})
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 8080, function () {
 	console.log('Twitch streamers now running...')
-});
+})
